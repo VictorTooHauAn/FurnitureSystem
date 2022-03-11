d8665dac656764829d6a79ca2d9fb693df2bec40
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-11 00:20:17 +0000

    created furniture test class

---
+++ FurnitureTesting/tstFurniture.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FurnitureTesting
+{
+    [TestClass]
+    public class tstFurniture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
