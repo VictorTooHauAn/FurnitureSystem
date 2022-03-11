5b7daa95d2846c11fb6229d2f8d410fcd30215ed
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-11 01:15:49 +0000

    added Find method to FurnitureClasses

--- FurnitureClasses/clsFurniture.cs
+++ FurnitureClasses/clsFurniture.cs
@@ -1,6 +1,12 @@
-﻿namespace FurnitureClasses
+﻿using System;
+
+namespace FurnitureClasses
 {
     public class clsFurniture
     {
+        public bool Find(int furnitureId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
