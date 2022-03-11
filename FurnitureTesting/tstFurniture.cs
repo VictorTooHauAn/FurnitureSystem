947a8393c961b34c3b91782e3d6d1505573011b1
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-11 00:29:01 +0000

    added FindMethodOk() to test furnture class

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -7,8 +7,18 @@ namespace FurnitureTesting
     public class tstFurniture
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FindMethodOk()
         {
+            // create an instance of the class we want to create
+            clsFurniture aFurniture = new clsFurniture();
+            // boolean variable to store the results of the validation
+            Boolean Found = false;
+            // create some test data to use with the method
+            Int32 FurnitureId = 1;
+            // invoke the method
+            Found = aFurniture.Find(FurnitureId);
+            // test to see if the result is true
+            Assert.IsTrue(Found);
         }
     }
 }
