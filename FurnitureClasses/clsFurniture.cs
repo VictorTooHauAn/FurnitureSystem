bac860e9f517e54737445a5372a7cfbe10adaf2f
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-11 22:01:34 +0000

    added private data member mFurnitureId to clsFurniture

--- FurnitureClasses/clsFurniture.cs
+++ FurnitureClasses/clsFurniture.cs
@@ -4,6 +4,9 @@ namespace FurnitureClasses
 {
     public class clsFurniture
     {
+        // private data member for the furniture id
+        private Int32 mFurnitureID;
+
         public int FurnitureId { get; set; }
 
         public bool Find(int furnitureId)
