ea92b4f7d468c2a3385ab65b27befab931f4d570
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-11 22:50:35 +0000

    modified getter and setter of public property of Furniture id

--- FurnitureClasses/clsFurniture.cs
+++ FurnitureClasses/clsFurniture.cs
@@ -5,9 +5,22 @@ namespace FurnitureClasses
     public class clsFurniture
     {
         // private data member for the furniture id
-        private Int32 mFurnitureID;
+        private Int32 mFurnitureId;
 
-        public int FurnitureId { get; set; }
+        // furniture id public data member
+        public int FurnitureId
+        {
+            get
+            {
+                // this line of sends data out of the property
+                return mFurnitureId;
+            }
+            set
+            {
+                // this line of code allows data into the property
+                mFurnitureId = value;
+            }
+        }
 
         public bool Find(int furnitureId)
         {
