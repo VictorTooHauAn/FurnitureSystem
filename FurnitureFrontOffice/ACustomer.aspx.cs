e781291ca2f406af861f7d6ee43d173a7898822f
VictorTooHauAn <victortoo@hotmail.com>
2022-03-13 17:30:08 +0000

    Added Testing and Customer Viewer for Customer class

---
+++ FurnitureFrontOffice/ACustomer.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FurnitureClasses;
+
+namespace FurnitureFrontOffice
+{
+    public partial class ACustomer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnOK_Click(object sender, EventArgs e)
+        {
+            //Create an instance of class
+            clsCustomer ACustomer = new clsCustomer();
+            //capture the Customer ID
+            ACustomer.CustomerUserID = txtCustomerUserID.Text;
+            //store the Customer ID in the session Object
+            Session["ACustomer"] = ACustomer;
+            //redirect to the viewer page
+            Response.Redirect("CustomerViewer.aspx");
+        }
+    }
+}
