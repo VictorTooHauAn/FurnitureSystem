7ed76c4ce7b62560dba47c717324ef578808e2da
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-17 17:19:35 +0000

    added find button to AFurniture web form

--- FurnitureFrontOffice/AFurniture.aspx.designer.cs
+++ FurnitureFrontOffice/AFurniture.aspx.designer.cs
@@ -148,5 +148,14 @@ namespace FurnitureFrontOffice
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox7;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
