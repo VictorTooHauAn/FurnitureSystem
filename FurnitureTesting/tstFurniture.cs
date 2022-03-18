88dbb362fd4ecbf7e91c64219dfecf3b32f93118
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-18 21:06:40 +0000

    added CategoryMid() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -418,5 +418,23 @@ namespace FurnitureTesting
             // test to see that the result is correct
             Assert.AreEqual(Error, "");
         }
+
+        [TestMethod]
+        // validation test for category mid
+        public void CategoryMid()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            string Category = "ko"; // should trigger an error
+            // add characters till it reaches the 14
+            Category = Category.PadRight(5, 'o');
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
     }
 }
