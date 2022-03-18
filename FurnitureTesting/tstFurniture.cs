7b73c1e07ce2063e6e044fdc69c9fe1275c0c56b
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-18 21:36:03 +0000

    added CategoryExtremeMax() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -454,5 +454,23 @@ namespace FurnitureTesting
             // test to see that the result is correct
             Assert.AreNotEqual(Error, "");
         }
+
+        [TestMethod]
+        // validation test for category plus one
+        public void CategoryExtremeMax()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            string Category = "ko";
+            // add characters till it reaches the 14
+            Category = Category.PadRight(100, 'o');
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
     }
 }
