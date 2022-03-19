2126a0bbfd9fb77bbb80dbd8ffcf840d0a7bb98d
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-19 22:11:24 +0000

    added NameMid() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -558,5 +558,24 @@ namespace FurnitureTesting
             Assert.AreEqual(Error, "");
         }
 
+
+        [TestMethod]
+        // validation test for name mid
+        public void NameMid()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            string Name = "";
+            // add characters to the variable
+            Name = Name.PadRight(7, 'o');
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
     }
 }
