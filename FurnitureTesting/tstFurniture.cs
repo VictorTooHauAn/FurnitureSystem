e099c0df75f9464e2acc9ad2f93cd04c0d8b7485
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-19 22:08:59 +0000

    added NameMax() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -540,5 +540,23 @@ namespace FurnitureTesting
             Assert.AreEqual(Error, "");
         }
 
+        [TestMethod]
+        // validation test for name max
+        public void NameMax()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            string Name = ""; // should be ok
+            // add characters to the variable
+            Name = Name.PadRight(15, 'p');
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
+
     }
 }
