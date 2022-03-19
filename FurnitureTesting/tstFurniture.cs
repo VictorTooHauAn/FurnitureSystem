a48aa49739cfef36bb32d0c9565d390ff60cb98a
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-19 22:28:59 +0000

    added WoodTypeMinPlusOne() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -646,5 +646,21 @@ namespace FurnitureTesting
             // test to see that the result is correct
             Assert.AreEqual(Error, "");
         }
+
+        [TestMethod]
+        // validation test for wood type min plus one
+        public void WoodTypeMinPLusOne()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            string WoodType = "so"; // should should be ok
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
     }
 }
