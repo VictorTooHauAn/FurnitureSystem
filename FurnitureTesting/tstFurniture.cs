fc89d3639996496c84d2f626aa830df26382adae
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-21 01:05:15 +0000

    added ColourExtremeMax() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -876,5 +876,23 @@ namespace FurnitureTesting
             Assert.AreNotEqual(Error, "");
         }
 
+        [TestMethod]
+        // validation test for colour max plus one
+        public void ColourExtremeMax()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            string Colour = "";
+            // add characters till it reaches the 14
+            Colour = Colour.PadRight(100, 'b');
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
+
     }
 }
