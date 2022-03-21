da010cf26cfe726651433f4820c941572715b80d
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-21 01:44:51 +0000

    added SizeExtremeMax() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -989,5 +989,21 @@ namespace FurnitureTesting
             // test to see that the result is correct
             Assert.AreNotEqual(Error, "");
         }
+
+        [TestMethod]
+        // validation test for size extreme max
+        public void SizeExtremeMax()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            double Size = 200.00;
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreNotEqual(Error, "");
+        }
     }
 }
