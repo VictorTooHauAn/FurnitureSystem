20eb986eace87a5bf4959bb78d4754626edc208c
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-21 01:21:11 +0000

    added SizeMinPlusOne() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -926,5 +926,20 @@ namespace FurnitureTesting
             Assert.AreEqual(Error, "");
         }
 
+        [TestMethod]
+        // validation test for size min plus one
+        public void SizeMinPlusOne()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            double Size = 8.4;
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
     }
 }
