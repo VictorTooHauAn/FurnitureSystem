9c7a890389c5a66887c2f6a7c12543a438ae3a3e
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-21 00:38:43 +0000

    added WoodTypeMaxPlusOne() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -662,5 +662,24 @@ namespace FurnitureTesting
             // test to see that the result is correct
             Assert.AreEqual(Error, "");
         }
+
+
+        [TestMethod]
+        // validation test for name max less one
+        public void WoodTypeMaxLessOne()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            string WoodType = ""; // should should be ok
+            // add characters to the variable
+            WoodType = WoodType.PadRight(14, 's');
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
     }
 }
