f660dec79cfdb601a3febd02da894d455c2740b6
Jamachi Udokporo <P2448622@my365.dmu.ac.uk>
2022-03-21 20:26:45 +0000

    added PriceMaxLessOne() test

--- FurnitureTesting/tstFurniture.cs
+++ FurnitureTesting/tstFurniture.cs
@@ -1181,5 +1181,21 @@ namespace FurnitureTesting
             // test to see that the result is correct
             Assert.AreEqual(Error, "");
         }
+
+        [TestMethod]
+        // validation test for price max
+        public void PriceMaxLessone()
+        {
+            // create an instance of the class we want to create
+            clsFurniture AFurniture = new clsFurniture();
+            // string variable to store any error message
+            String Error = "";
+            // create some test data to pass to the method
+            double Price = 299999.00;
+            // invoke the method
+            Error = AFurniture.Valid(Category, Name, WoodType, Colour, Size, StockNo, Price);
+            // test to see that the result is correct
+            Assert.AreEqual(Error, "");
+        }
     }
 }
