7b41ec630f4bc70d9d3bf08e5c014e8222c9c9b8
VictorTooHauAn <victortoo@hotmail.com>
2022-03-22 21:31:40 +0000

    add constructor

--- FurnitureClasses/clsCustomerCollection.cs
+++ FurnitureClasses/clsCustomerCollection.cs
@@ -1,9 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace FurnitureClasses
 {
     public class clsCustomerCollection
     {
+        //constructor for the class
+        public clsCustomerCollection()
+        {
+            //create the items of test data
+            clsCustomer TestItem = new clsCustomer();
+            //set its properties
+            TestItem.CustomerUserID = "12345";
+            TestItem.Firstname = "Peter";
+            TestItem.Lastname = "Lee";
+            TestItem.Address = "5, London";
+            TestItem.EmailAddress = "peter@gmail.com";
+            TestItem.PhoneNumber = "+4498765432123";
+            TestItem.DateAdded = DateTime.Now.Date;
+            //add the item to the test list
+            mCustomerList.Add(TestItem);
+            //re initialise the object for some new data
+            TestItem = new clsCustomer();
+            //set its properties
+            TestItem.CustomerUserID = "54321";
+            TestItem.Firstname = "Henry";
+            TestItem.Lastname = "Jas";
+            TestItem.Address = "53, Liverpool";
+            TestItem.EmailAddress = "henry@gmail.com";
+            TestItem.PhoneNumber = "+447123123123";
+            TestItem.DateAdded = DateTime.Now.Date;
+            //add the item to thes test list
+            mCustomerList.Add(TestItem);
+        }
         //private data member for the list
         List<clsCustomer> mCustomerList = new List<clsCustomer>();
         public List<clsCustomer> CustomerList 
@@ -31,5 +60,7 @@ namespace FurnitureClasses
             }
         }
         public clsCustomer ThisCustomer { get; set; }
+
+      
     }
 }
--- FurnitureTesting/tstCustomerCollection.cs
+++ FurnitureTesting/tstCustomerCollection.cs
@@ -50,7 +50,7 @@ namespace FurnitureTesting
             //create an instance of the class we want to create
             clsCustomerCollection AllCustomer = new clsCustomerCollection();
             //create some test data to assign to the property
-            Int32 SomeCount = 0;
+            Int32 SomeCount = 2;
             //assign the data to the property
             AllCustomer.Count = SomeCount;
             //test to see that the two values are the same
