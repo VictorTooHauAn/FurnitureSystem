2a5b1882f3e7a3324c13655df56bf95ab3fcad42
VictorTooHauAn <victortoo@hotmail.com>
2022-03-22 10:41:22 +0000

    Add the find method for the presentation layer

--- FurnitureFrontOffice/ACustomer.aspx.cs
+++ FurnitureFrontOffice/ACustomer.aspx.cs
@@ -26,5 +26,30 @@ namespace FurnitureFrontOffice
             //redirect to the viewer page
             Response.Redirect("CustomerViewer.aspx");
         }
+
+        protected void btnFind_Click(object sender, EventArgs e)
+        {
+            //Create an instance of class
+            clsCustomer ACustomer = new clsCustomer();
+            //Variable to store the primary key
+            string CustomerUserID;
+            //Variable to store the result of the find operation
+            Boolean Found = false;
+            //get the primary key entered by the user
+            CustomerUserID = Convert.ToString(txtCustomerUserID.Text);
+            //find the record
+            Found = ACustomer.Find(CustomerUserID);
+            //if found
+            if (Found == true)
+            {
+                //display the values of properties
+                txtAddress.Text = ACustomer.Address;
+                txtDateOfBirth.Text = ACustomer.DateAdded.ToString();
+                txtEmailAddress.Text = ACustomer.EmailAddress;
+                txtFirstName.Text = ACustomer.Firstname;
+                txtLastName.Text = ACustomer.Lastname;
+                txtPhoneNumber.Text = ACustomer.PhoneNumber;
+            }
+        }
     }
 }
