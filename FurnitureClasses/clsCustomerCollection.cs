7ba7df6a27e81989a401ae5d9835e90962b90979
VictorTooHauAn <victortoo@hotmail.com>
2022-03-22 20:16:39 +0000

    Add the method for the CustomerList, CountProperty and ThisCustomer

---
+++ FurnitureClasses/clsCustomerCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FurnitureClasses
+{
+    public class clsCustomerCollection
+    {
+        public List<clsCustomer> CustomerList { get; set; }
+        public int Count { get; set; }
+        public clsCustomer ThisCustomer { get; set; }
+    }
+}
